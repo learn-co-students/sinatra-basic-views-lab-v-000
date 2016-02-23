9c9808659ddec32159e6bc681769bab1c831f6b0
zacscodingclub <zbaston@gmail.com>
2016-02-22 18:33:14 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
+  get '/' do
+    @bowling_alley = BowlingAlley.new(" Buster's Best Bowling Alley"," Bet You Can't Bowl A Strike Every Time"," 97 Bowling Alley Lane, Boulder CO ")
+    erb :index
+  end
 end
