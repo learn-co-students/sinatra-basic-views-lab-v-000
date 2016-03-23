733caf96526f3bdbff2e8a6df58f79894cbb4515
agdavid <antonio.g.david@gmail.com>
2016-03-23 11:12:12 -0400

    code basic route and view for Bowling Alley domain in Sinatra

--- app.rb
+++ app.rb
@@ -2,5 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    #renders the view index.erb, upon receipt of root URL request
+    erb :index 
+  end
+
 
 end
