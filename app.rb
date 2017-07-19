583f7a8e522f80df84ebb46d48b36814262e9acf
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-07-19 03:43:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do  
+    erb :index
+  end
 
+  get "/regular-hours" do
+    erb :regular_hours
+  end
+
+  get "/dates-of-tournaments" do
+    erb :dates_of_tournaments
+  end
+
+  get "/bar-menu" do
+    erb :bar_menu
+  end
 
 end
