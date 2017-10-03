65d465530f5de482b713a5d30edf4702bffe1cc7
Frederico  Morato <freddymorato@gmail.com>
2017-10-03 00:52:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    status 200
+    erb :index
+  end
 
+  get "/name" do
+    "<h1> Buster's Best Bowling Alley</h1>"
+  end
+
+  get "/tagline" do
+    "<h2> Bet You Can't Bowl A Strike Every Time</h2>"
+  end
+
+  get "/address" do
+    "<p> 97 Bowling Alley Lane, Boulder CO </p>"
+  end
 end
