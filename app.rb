a4c8f8e91780ae563851e1fff17b7701e0272f42
Alex <github email address>
2017-11-28 20:29:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,8 +5,5 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-	get "/info" do  #this shows that the url mapping will register off of /info
-		erb :info #all this section has to do is match the name of the views file
-	end
-
+	
 end
