aa935762f8b5c6303b9c771372290d378e829b79
Alex <github email address>
2017-11-28 20:24:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	get '/' do
+		erb :index
+	end
+	get "/info" do  #this shows that the url mapping will register off of /info
+		erb :info #all this section has to do is match the name of the views file
+	end
 
 end
