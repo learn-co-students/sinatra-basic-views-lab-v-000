b00edd13c0682f9bfff343161eca637478fc059d
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-02-20 01:38:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require_relative 'config/environment'
-
+#========================================== 
 class App < Sinatra::Base
-
-
+#==================routes================== 
+  # index
+  get "/" do
+  	erb :index
+  end
+#========================================== 
 end
