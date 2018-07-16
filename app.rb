830cc1af1ec2f3ef948a1317998433b27aa77841
Kachel Wittig <kjwittig@gmail.com>
2018-07-15 20:54:26 -0500

    passed dem tests

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/name' do
+    "<h1>Buster's Best Bowling Alley</h1>"
+  end
+
+  
 
 end
