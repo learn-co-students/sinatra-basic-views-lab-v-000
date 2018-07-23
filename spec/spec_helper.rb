6662632c7bde35962b191315c1413888086c8a4d
github username <github email address>
2018-07-22 21:18:00 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,15 @@
-require_relative '../config/environment.rb'
-require 'rack/test'
-RACK_ENV = "test"
-ENV["RACK_ENV"] = "test"
+#require_relative '../config/environment.rb'
+#require 'rack/test'
+#RACK_ENV = "test"
+#ENV["RACK_ENV"] = "test"
 
-RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.include Rack::Test::Methods
+#RSpec.configure do |config|
+#  config.run_all_when_everything_filtered = true
+#  config.filter_run :focus
+#  config.include Rack::Test::Methods
 
-  config.order = 'default'
-end
-
-def app
-  Rack::Builder.parse_file('config.ru').first
-end
+#  config.order = 'default'
+#
+#def app
+#  Rack::Builder.parse_file('config.ru').first
+#
