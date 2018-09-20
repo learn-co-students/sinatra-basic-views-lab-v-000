727030f9a4fdc7d8a121b0c64327c829fc1687f9
Nate Dietz <nsdietz@gmail.com>
2018-09-20 07:19:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/hours' do
+    erb :hours
+  end
+
+  get '/dates' do
+    erb :dates
+  end
+
+  get '/menu' do
+    erb :menu
+  end
 
 end
