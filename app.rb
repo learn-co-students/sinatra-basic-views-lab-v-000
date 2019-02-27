ec0fe1231e28f88babb877bbfcebe54062eacae3
Cheryl K <cherylkelson@icloud.com>
2019-02-27 22:11:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,10 +1,13 @@
 require_relative 'config/environment'
 
-require_relative 'config/environment'
-
 class App < Sinatra::Base
 
   get '/' do
     erb :index
   end
+
+  get '/address'do
+    erb :address 
+  end
+
 end
